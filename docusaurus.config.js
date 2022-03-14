e2c8365c9e7e919fdad3124f7045aa6bc9805eb3
Jim Wheeler <jwheele1@gmail.com>
2022-03-14 15:21:02 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "IRMI",
-  tagline: "Documentation for internal processes",
+  title: "IRMI Learning",
+  tagline: "Learning center for internal processes",
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: "/",
   onBrokenLinks: "throw",
@@ -52,7 +52,7 @@ const config = {
         },
         items: [
           {
-            "label": "Documentation",
+            "label": "Learning",
             "to": "docs"
           },
           {
