3652560f05c581a2cf997fcef89f1ad5d3b0a8f4
Jim Wheeler <jwheele1@gmail.com>
2023-03-23 15:44:29 -0500

    Troubleshooting .env file

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,41 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs">
-            Docs
-          </Link>
-          <Link className="button button--secondary button--lg" to="/docs/faq">
-            FAQs
-          </Link>
+        {/* <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+        {/* <div class="container"> */}
+        <div class="row">
+          <div class="col">
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link className="button button--secondary button--lg" to="/docs">
+                Docs
+              </Link>
+              <Link
+                className="button button--secondary button--lg"
+                to="/docs/faq"
+              >
+                FAQs
+              </Link>
+            </div>
+          </div>
+          <div class="col">
+            <iframe
+              src={
+                `https://webchat.botframework.com/embed/${process.env.REACT_APP_CHATBOT_SECRET_ONE}?s=${process.env.REACT_APP_CHATBOT_SECRET_TWO}`
+              }
+            ></iframe>
+          </div>
         </div>
       </div>
+      {/* </div> */}
     </header>
   );
 }
