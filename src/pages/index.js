7e7a9e3b79072096b8071f05fa1aa836ea1efad7
Jim Wheeler <jwheele1@gmail.com>
2023-04-01 11:15:50 -0500

    Changed values from dev to production

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ function HomepageHeader() {
           <div class="col">
             <iframe
               src={
-                `https://webchat.botframework.com/embed/${siteConfig.customFields.first_key}?s=${siteConfig.customFields.second_key}`
+                "https://webchat.botframework.com/embed/JimVALUEONE?s=JimVALUETWO"
               }
             ></iframe>
           </div>
